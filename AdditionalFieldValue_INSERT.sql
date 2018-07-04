572e9bfef4703897d009ab8b062e5b93962c0998
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2018-07-04 10:58:15 +0200

    Aligned the file properly

--- AdditionalFieldValue_INSERT.sql
+++ AdditionalFieldValue_INSERT.sql
@@ -12,36 +12,36 @@ AS
 BEGIN
        SET NOCOUNT ON;
  
-		/* DECLARE VARIABLES */
-       DECLARE @Trackingnummer varchar(255)
-	   DECLARE @Logistiker varchar(255)
-	   DECLARE @Belgnummer varchar(255)
-	   DECLARE @TableIDAuftrag UNIQUEIDENTIFIER
+	/* DECLARE VARIABLES */
+        DECLARE @Trackingnummer varchar(255)
+	DECLARE @Logistiker varchar(255)
+	DECLARE @Belgnummer varchar(255)
+	DECLARE @TableIDAuftrag UNIQUEIDENTIFIER
  
-		/* GET TRACKINGNUMBER */
-       SELECT @Trackingnummer = INSERTED.ValueString       
-       FROM INSERTED WHERE INSERTED.DefRowID = '{BCE33303-72C2-11E8-9EBF-000C29018628}'
+	/* GET TRACKINGNUMBER */
+        SELECT @Trackingnummer = INSERTED.ValueString       
+        FROM INSERTED WHERE INSERTED.DefRowID = '{BCE33303-72C2-11E8-9EBF-000C29018628}'
 
-	    /* GET LOGISTIKER */
-	   SELECT @Logistiker = INSERTED.ValueString       
-       FROM INSERTED WHERE INSERTED.DefRowID = '{E7C59F43-72C2-11E8-9EBF-000C29018628}'
+	/* GET LOGISTIKER */
+	SELECT @Logistiker = INSERTED.ValueString       
+        FROM INSERTED WHERE INSERTED.DefRowID = '{E7C59F43-72C2-11E8-9EBF-000C29018628}'
 
-	   /* GET BELEGNUMMER um auf dessen Basis die ROWID des Auftrags zu erhalten */
-	   SELECT @Belgnummer = [dbo].[BESTELLKOPF].[AUFTRAGSNR]
-	   FROM [dbo].[BESTELLKOPF], INSERTED
-	   WHERE [dbo].[BESTELLKOPF].[FSROWID] = INSERTED.TableRowID
+	 /* GET BELEGNUMMER um auf dessen Basis die ROWID des Auftrags zu erhalten */
+	 SELECT @Belgnummer = [dbo].[BESTELLKOPF].[AUFTRAGSNR]
+	 FROM [dbo].[BESTELLKOPF], INSERTED
+	 WHERE [dbo].[BESTELLKOPF].[FSROWID] = INSERTED.TableRowID
 
-	   /* GET AUFTRAG-FSROWID */
-	   SELECT @TableIDAuftrag = [dbo].[AUFTRAGSKOPF].[FSROWID]
-	   FROM [dbo].[AUFTRAGSKOPF]
-	   WHERE [dbo].[AUFTRAGSKOPF].[BELEGNR] = @Belgnummer
+	  /* GET AUFTRAG-FSROWID */
+	  SELECT @TableIDAuftrag = [dbo].[AUFTRAGSKOPF].[FSROWID]
+	  FROM [dbo].[AUFTRAGSKOPF]
+	  WHERE [dbo].[AUFTRAGSKOPF].[BELEGNR] = @Belgnummer
  
-		/* INSERT IF NOT NULL */
-		IF @Trackingnummer IS NOT NULL
-			INSERT INTO [dbo].[AdditionalFieldValue] (DefRowID, TableRowID, ValueString)
-			VALUES('{BEF38EDC-2DBF-11E8-949A-000C29018628}', @TableIDAuftrag, @Trackingnummer)
-		ELSE IF @Logistiker IS NOT NULL
-			INSERT INTO [dbo].[AdditionalFieldValue] (DefRowID, TableRowID, ValueString)
-			VALUES('{91B2ACAD-791A-11E8-BB51-000C29018628}', @TableIDAuftrag, @Logistiker)      
+	  /* INSERT IF NOT NULL */
+	  IF @Trackingnummer IS NOT NULL
+		INSERT INTO [dbo].[AdditionalFieldValue] (DefRowID, TableRowID, ValueString)
+		VALUES('{BEF38EDC-2DBF-11E8-949A-000C29018628}', @TableIDAuftrag, @Trackingnummer)
+	  ELSE IF @Logistiker IS NOT NULL
+		INSERT INTO [dbo].[AdditionalFieldValue] (DefRowID, TableRowID, ValueString)
+		VALUES('{91B2ACAD-791A-11E8-BB51-000C29018628}', @TableIDAuftrag, @Logistiker)      
 
 END
