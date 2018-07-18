8e3bff6421cd05ecc6eb46f790703b022a0eb1ac
ADarkHero <maik.riedlsperger@gmail.com>
2018-07-18 10:52:24 +0200

    Added KUNDEN_INSERT

---
+++ KUNDEN_INSERT.sql
@@ -0,0 +1,25 @@
+USE [LOE01]
+GO
+/****** Object:  Trigger [dbo].[AdditionalFieldValue_INSERT]    Script Date: 18.07.2018 10:35:41 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER TRIGGER [dbo].[KUNDEN_INSERT]
+       ON [dbo].[KUNDEN]
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+	DECLARE @Kundennr decimal(15, 0)
+	DECLARE @Kundengruppe varchar(8)
+
+	SELECT @Kundennr = INSERTED.KUNDENNR       
+       FROM INSERTED
+
+	SELECT @Kundengruppe = INSERTED.GRUPPE       
+       FROM INSERTED
+
+	   IF @Kundengruppe = '400'
+			UPDATE [dbo].[KUNDEN] SET SPERRUNG = 0
+END
