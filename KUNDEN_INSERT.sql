fc860dae6bceeee39983645f66bc4224b39670cf
ADarkHero <maik.riedlsperger@gmail.com>
2018-07-30 11:01:18 +0200

    Added support for Zahlungsnr

--- KUNDEN_INSERT.sql
+++ KUNDEN_INSERT.sql
@@ -1,6 +1,6 @@
 USE [LOE01]
 GO
-/****** Object:  Trigger [dbo].[AdditionalFieldValue_INSERT]    Script Date: 18.07.2018 10:35:41 ******/
+/****** Object:  Trigger [dbo].[KUNDEN_INSERT]    Script Date: 30.07.2018 10:07:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,6 +13,7 @@ BEGIN
     SET NOCOUNT ON;
 	DECLARE @Kundennr decimal(15, 0)
 	DECLARE @Kundengruppe varchar(8)
+	DECLARE @Zahlungsnr smallint
 
 	SELECT @Kundennr = INSERTED.KUNDENNR       
        FROM INSERTED
@@ -20,7 +21,14 @@ BEGIN
 	SELECT @Kundengruppe = INSERTED.GRUPPE       
        FROM INSERTED
 
+	SELECT @Zahlungsnr = INSERTED.ZBNUMMER
+	   FROM INSERTED
+
 	   /* Wenn die Kundengruppe NW-Shop ist, Gesamtsperre aufheben */
 	   IF @Kundengruppe = '400'
 			UPDATE [dbo].[KUNDEN] SET SPERRUNG = 0 WHERE KUNDENNR = @Kundennr
+
+		/* Wenn die Zahlungsnummer leer ist, Zahlungsnummer auf Vorkasse setzen */
+		IF @Zahlungsnr IS NULL
+			UPDATE [dbo].[KUNDEN] SET ZBNUMMER = 10 WHERE KUNDENNR = @Kundennr
 END
