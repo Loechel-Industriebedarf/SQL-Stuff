726ae2706cadc35dc2e1eecc88d2807920b0e75c
ADarkHero <maik.riedlsperger@gmail.com>
2018-08-09 11:01:52 +0200

    Added view for articles
    Workaround for access not supporting more than 255 columns

---
+++ view_ARTIKEL.sql
@@ -0,0 +1,11 @@
+SELECT       FSROWID, ROWID, ARTIKELNR, NVH_NoPicking, NVH_AutoArticleTextToSO, P53_SpecialPriceMemo, P116_CatalogPage, P116_CodeEAN, P116_NoSerialAssign, P116L_ProducerID, MinGrossYieldAmount, OptimalLotSize, 
+			 ReturnsNoteID, NoCashDiscountPurchase, FeatureClassID, NVH_DefaultLengthForCut, P116LI_Picturefile1, P116LI_Picturefile2, P116LI_Documentfile1, P116LI_Documentfile2, P116LI_Picturefile3, P116LI_Picturefile4, 
+			 P116LI_PictureTyp, P116LI_InsideDiameter, P116LI_OutsideDiameter, P116LI_Producer_Name, P116LI_CASNr2, P116LI_HLKCode, P116LI_PVAvailability, P116LI_PVAlterationDateTime, P116LI_NoEcommerce5, 
+			 P116LI_NoEcommerce6, P116LI_NoEcommerce7, P116LI_NoEcommerce8, P116LI_PVSupp_AID, BasicQuantityUnit, BasicContentQuantity, NVH_NoQuantityCalcUnit, ManufacturerId, ManufacturerProductId, 
+			 ManufacturerProductName, NoManSurchPos, NVH_ExcludeFromSurcharge, P116LI_ApplicationArea, P116LI_BatteryCapacity, P116LI_BatteryCount, P116LI_BatteryType, P116LI_BatteryVolt, P116LI_Color, P116LI_Content, 
+			 P116LI_Drilling, P116LI_Drive, P116LI_Equipment, P116LI_Execution, P116LI_Feature, P116LI_Hardness, P116LI_HoleSpacing, P116LI_Load, P116LI_Material, P116LI_Norm, P116LI_RotationMin, P116LI_Series, 
+			 P116LI_ShaftDiameter, P116LI_Size, P116LI_Stability, P116LI_Strength, P116LI_Surface, P116LI_SwordLength, P116LI_TeethCount, P116LI_TempMax, P116LI_TempMin, P116LI_Tolerance, P116LI_Torque, P116LI_Type, 
+			 P116LI_WorkingPressure, DispatchRoute, FactorVolume, GTIN, HideOldBatches, ItemsListPriceOnSubItem, ManufactoringLength, NoBatchSplit, NoIEUpdate, NVH_CutReplyMatchQuantity, NVH_LoadingEquipTypeID, 
+			 NVH_MFBatchCuttingReply, NVH_MFLengthCorr, NVH_MFMaterialType, NVH_MFSingleBatch, NVH_MFType, NVH_TransOnlyNecessaryQuant, SILengthUnitID, SIWeightUnitID, ValidSupplierCondition, P116LI_GHSSignal, 
+			 NVH_PurchaseOrderBlock
+FROM         dbo.ARTIKEL
