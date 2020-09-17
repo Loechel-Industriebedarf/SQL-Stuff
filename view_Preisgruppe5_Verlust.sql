605b2ffd5b642ea1eba9b7410e41da5fc9453ea2
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2020-09-17 16:29:38 +0200

    Added geloescht

--- view_Preisgruppe5_Verlust.sql
+++ view_Preisgruppe5_Verlust.sql
@@ -6,4 +6,4 @@ FROM            dbo.View_VK5Preise INNER JOIN
                                                          ELSE CASE WHEN ARTIKEL_1.KEK > 200 THEN 1.03 ELSE CASE WHEN ARTIKEL_1.KEK > 100 THEN 1.05 ELSE CASE WHEN ARTIKEL_1.KEK > 50 THEN 1.1 ELSE 1.15 END END END END AS CALC
                                FROM            dbo.ARTIKEL AS ARTIKEL_1) AS cte ON dbo.View_VK5Preise.ARTIKELNR = cte.ARTIKELNR AND (cte.KEKLEK * cte.CALC < dbo.View_VK5Preise.VK5 - 0.01 OR
                          cte.KEKLEK * cte.CALC > dbo.View_VK5Preise.VK5 + 0.01) AND dbo.ARTIKEL.P53_SpecialPriceMemo IS NULL AND (dbo.ARTIKEL.P116LI_NoEcommerce5 = 0 OR
-                         dbo.ARTIKEL.P116LI_NoEcommerce5 IS NULL)
+                         dbo.ARTIKEL.P116LI_NoEcommerce5 IS NULL) AND dbo.ARTIKEL.GELOESCHT <> 1
