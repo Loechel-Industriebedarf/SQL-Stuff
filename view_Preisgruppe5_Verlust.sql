948d28509424b7b9745b20cbbfd7362f7f414ab5
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2020-09-17 17:00:14 +0200

    Null and 0 are stupid.

--- view_Preisgruppe5_Verlust.sql
+++ view_Preisgruppe5_Verlust.sql
@@ -5,5 +5,6 @@ FROM            dbo.View_VK5Preise INNER JOIN
                                                          THEN CAST(REPLACE(ARTIKEL_1.P116LI_RotationMin, ',', '.') AS float) 
                                                          ELSE CASE WHEN ARTIKEL_1.KEK > 200 THEN 1.03 ELSE CASE WHEN ARTIKEL_1.KEK > 100 THEN 1.05 ELSE CASE WHEN ARTIKEL_1.KEK > 50 THEN 1.1 ELSE 1.15 END END END END AS CALC
                                FROM            dbo.ARTIKEL AS ARTIKEL_1) AS cte ON dbo.View_VK5Preise.ARTIKELNR = cte.ARTIKELNR AND (cte.KEKLEK * cte.CALC < dbo.View_VK5Preise.VK5 - 0.01 OR
-                         cte.KEKLEK * cte.CALC > dbo.View_VK5Preise.VK5 + 0.01) AND dbo.ARTIKEL.P53_SpecialPriceMemo IS NULL AND (dbo.ARTIKEL.P116LI_NoEcommerce5 = 0 OR
-                         dbo.ARTIKEL.P116LI_NoEcommerce5 IS NULL) AND dbo.ARTIKEL.GELOESCHT <> 1
+                         cte.KEKLEK * cte.CALC > dbo.View_VK5Preise.VK5 + 0.01) AND dbo.ARTIKEL.P53_SpecialPriceMemo IS NULL AND (dbo.ARTIKEL.P116LI_NoEcommerce5 IS NULL OR
+                         dbo.ARTIKEL.P116LI_NoEcommerce5 = 0) AND (dbo.ARTIKEL.GELOESCHT IS NULL OR
+                         dbo.ARTIKEL.GELOESCHT = 0)
