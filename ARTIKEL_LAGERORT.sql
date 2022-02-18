60437912d77b7f530d19cddada0e4f22b4fcd1ba
ADarkHero <maik.riedlsperger@gmail.com>
2022-02-18 14:45:30 +0100

    Added removal of spaces to lagerort

--- ARTIKEL_LAGERORT.sql
+++ ARTIKEL_LAGERORT.sql
@@ -1,6 +1,6 @@
 USE [LOE01]
 GO
-/****** Object:  Trigger [dbo].[ARTIKEL_LAGERORT]    Script Date: 02.09.2021 08:55:25 ******/
+/****** Object:  Trigger [dbo].[ARTIKEL_LAGERORT]    Script Date: 18.02.2022 14:31:09 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -18,10 +18,12 @@ BEGIN
 	-- Erste 8 Zeichen auslesen
 	-- + Zeichen entfernen
 	-- Leerzeichen an Anfang und Ende entfernen
+	-- Nach Leerzeichen abschneiden
 	SELECT @Lagerort = TRIM(REPLACE(SUBSTRING(INSERTED.P116LI_TempMax, 0, 9), '+', ''))
 		FROM INSERTED
+	SELECT @Lagerort = LEFT(@Lagerort, CHARINDEX(' ', @Lagerort) - 1)  FROM INSERTED where CHARINDEX(' ', @Lagerort) > 0
 	SELECT @Artikelnummer = INSERTED.ARTIKELNR
 		FROM INSERTED
 
-	UPDATE [dbo].[LAGER] SET LAGERPLATZ = @Lagerort WHERE ARTIKELNR = @Artikelnummer
+	UPDATE [dbo].[LAGER] SET LAGERPLATZ = @Lagerort WHERE ARTIKELNR = @Artikelnummer AND LAGERNR = 1
 END
